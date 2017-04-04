f009d339851ac9c66b401ddd2d3d549410eac80c
Mina Slater <slater.mina@gmail.com>
2017-04-04 15:17:17 -0500

    fixes a typo in character_data. derp.

--- character_parser.rb
+++ character_parser.rb
@@ -3,6 +3,6 @@ require_relative "json_parser"
 class CharacterParser < Parser
   def find_id(character_json)
     character_data = to_hash(character_json)
-    character_data["data"]["results"]["id"]
+    character_data["data"]["results"][0]["id"]
   end
 end
