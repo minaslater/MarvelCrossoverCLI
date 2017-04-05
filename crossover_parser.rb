394a48fa4433420a58515cb2309a163f1925b6cd
Mina Slater <slater.mina@gmail.com>
2017-04-04 21:19:12 -0500

    adds Crossover Parser class to get comic info

---
+++ crossover_parser.rb
@@ -0,0 +1,38 @@
+require_relative "json_parser"
+require "date"
+
+class CrossoverParser < Parser
+  attr_reader :comic_data
+
+  def initialize(comic_json)
+    @comic_data = to_hash(comic_json)
+  end
+
+  def find_title
+    comic_data["data"]["results"][0]["title"]
+  end
+
+  def find_issue
+    comic_data["data"]["results"][0]["issueNumber"]
+  end
+
+  def find_date
+    date = comic_data["data"]["results"][0]["dates"][0]["date"]
+    DateTime.parse(date).strftime("%B %d, %Y")
+  end
+
+  def find_creative_team
+    creative_team = {}
+    # number = comic_data["data"]["results"][0]["creators"]["available"]
+    # count = 0
+    # while count < number
+    team_members = comic_data["data"]["results"][0]["creators"]["items"]
+    team_members.each do |hash|
+      job = hash["role"]
+      name = hash["name"]
+      creative_team[job] = name
+      # count += 1
+    end
+    creative_team
+  end
+end
