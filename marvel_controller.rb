f9dbdd8fb7d9f20b41ee8418524bf27dad4154c4
Mina Slater <slater.mina@gmail.com>
2017-04-04 20:08:29 -0500

    adds Builder and Requestor methods with IDs

--- marvel_controller.rb
+++ marvel_controller.rb
@@ -1,18 +1,24 @@
 require_relative "requestor"
+require_relative "character_parser"
 
 class MarvelCLIController
-  attr_reader :requestor
+  attr_reader :requestor, :character_parser
 
   def initialize
+    character1 = "Captain America"
+    character2 = "Wolverine"
     # ask user for two characters
     # make requests to API for both characters
     @requestor = Requestor.new
-    character1_json = requestor.request(character1)
-    character2_json = requestor.request(character2)
+    character1_json = requestor.request_id(character1)
+    character2_json = requestor.request_id(character2)
     @character_parser = CharacterParser.new
     id1 = character_parser.find_id(character1_json)
     id2 = character_parser.find_id(character2_json)
     # compare comic appearances with another API request by id
+    comic_json = requestor.request_comic(id1, id2)
     # output matches
   end
 end
+
+MarvelCLIController.new
--- url_builder.rb
+++ url_builder.rb
@@ -2,19 +2,22 @@ require_relative "api_param_builder"
 require "uri"
 
 class URLBuilder
-  attr_reader :character_name
+  # def initialize(character_name)
+  #   @character_name = character_name
+  # end
 
-  def initialize(character_name)
-    @character_name = character_name
-  end
-
-  def character_to_s
+  def character_to_s(character_name)
     character_url = URI.escape(character_name)
     "&name=#{character_url}"
   end
 
-  def build
+  def build_with_name(character_name)
     params = APIParamBuilder.new
-    "http://gateway.marvel.com/v1/public/characters?" + params.to_s + character_to_s
+    "http://gateway.marvel.com/v1/public/characters?" + params.to_s + character_to_s(character_name)
+  end
+
+  def build_with_ids(id1, id2)
+    params = APIParamBuilder.new
+    "http://gateway.marvel.com/v1/public/characters/" + "#{id1}" + "/comics?" + params.to_s + "&sharedAppearances=#{id2}&orderBy=-onsaleDate&limit=1"
   end
 end
